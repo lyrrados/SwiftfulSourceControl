63a5bbf8390adf44b905af83c09451b9eb9d9567
lyrrados <damiantwells@gmail.com>
2025-07-15 11:07:36 -0500

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by AncientBlueDragon on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
