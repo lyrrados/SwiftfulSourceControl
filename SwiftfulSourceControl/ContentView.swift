13f238ef3313323dadac4e874b484059f95ba8c9
lyrrados <damiantwells@gmail.com>
2025-07-16 14:40:54 -0500

    Change Click me background color, change swiftful text

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -13,12 +13,12 @@ struct ContentView: View {
             Image(systemName: "heart.fill")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Swiftful Thinking!")
+            Text("Swiftful!")
             
             Button("Click me!") {
                 
             }
-            .background(.red)
+            .background(.green)
         }
         .padding()
     }
