9653d937fc2c46762bdd19de8127dcd215e9bcc7
lyrrados <damiantwells@gmail.com>
2025-07-16 14:54:03 -0500

    [Feature] Add Subscribe button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -34,6 +34,9 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Swiftful!")
+            Button("Subscribe") {
+                
+            }
             
             Button("Click me!") {
                 
